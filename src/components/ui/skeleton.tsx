10e2f08903b0e5ae643654ac2f316325edf7fa69
its-me-santosh <ymanoutube@gmail.com>
2026-03-30 22:55:55 +0545

    fix: Fix linting errors

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,7 +1,7 @@
 import type { HTMLAttributes } from "react";
 import { cn } from "../../lib/utils";
 
-interface SkeletonProps extends HTMLAttributes<HTMLDivElement> {}
+type SkeletonProps = HTMLAttributes<HTMLDivElement>;
 
 function Skeleton({ className, ...props }: SkeletonProps) {
   return (
